dc1a10b0dcc343238f8cf94e8ec0e86bfde3a0c1
Amit Manchanda <amit27manchanda@gmail.com>
2017-08-16 08:14:08 +0530

    initial app update

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
 
-get "welcome/skills"
-get "welcome/portfolio"
-get "welcome/contact"
-get "welcome/our_team"
+  devise_for :users
+
+
+match '/about_us',   to: 'pages#about_us',   via: :get
+match '/contact_us', to: 'pages#contact_us', via: :get
  
-root 'welcome#homepage'
+root 'search_schools#index'
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,55 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170704082928) do
+ActiveRecord::Schema.define(version: 20170816021227) do
+
+  create_table "RTEgrade", force: :cascade do |t|
+    t.integer "grade"
+  end
+
+  create_table "Searchschool", force: :cascade do |t|
+    t.string "name"
+    t.integer "schoolcode"
+    t.string "address1"
+    t.string "address2"
+    t.string "city"
+    t.string "state"
+    t.string "website"
+    t.string "phone"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.string "name"
+    t.integer "rating"
+  end
+
+  create_table "schoolcategory", force: :cascade do |t|
+    t.string "name"
+  end
+
+  create_table "schoolmanagement", force: :cascade do |t|
+    t.string "name"
+  end
+
+  create_table "schooltype", force: :cascade do |t|
+    t.string "name"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
 
 end
